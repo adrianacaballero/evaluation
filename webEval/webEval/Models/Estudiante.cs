87bf99e96b67752f4282e5a9501262f4f3055f00
visual_community@outlook.com <visual_community@outlook.com>
2019-09-26 19:56:46 -0400

    webapp estudiante

---
+++ webEval/webEval/Models/Estudiante.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace webEval.Models
+{
+    public enum Typestudent
+    {
+        Female,
+        Male
+    }
+    public class Estudiante
+    {
+        [Key]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public Typestudent Sex { get; set; }
+        [Required]
+        public int Birthdate { get; set; }
+        public virtual ICollection<Nota> Notas { get; set; }
+    } 
+}
---
+++ webEval/webEval/Models/Nota.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webEval.Models
+{
+    public enum TypeMateria
+    {
+        Matematicas,
+        Fisica,
+        Quimica
+    }
+    public enum TypeEstado
+    {
+        Aprobado,
+        Aplazado
+    }
+    public class Nota
+    {
+        [Key]
+        [Required]
+        public TypeMateria Materia { get; set; }
+        [Required]
+        public int Notas { get; set; }
+        [Required]
+        public TypeEstado Estado { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
